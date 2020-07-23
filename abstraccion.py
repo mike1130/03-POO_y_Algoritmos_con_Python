13192c206c3c8d50d5e4087627824a1a0dd2d2f1
Michel Caraballo <caraballomichel@gmail.com>
2020-07-23 07:42:24 -0500

    abstraccion.py de un ciclo de lavado

---
+++ abstraccion.py
@@ -0,0 +1,32 @@
+
+class Lavadora:
+
+    def __init__(self):
+        pass
+
+    def lavar(self, temperatura='caliente'):
+        self._llenar_tanque_de_agua(temperatura)
+        self._anadir_jabon()
+        self._lavar()
+        self._centrifugar()    
+
+    def _llenar_tanque_de_agua(self, temperatura):
+        print(f'Llenando el tanque con agua {temperatura} ºC')
+
+    def _anadir_jabon(self):
+        print('Añadiendo jabón')
+
+    def _lavar(self):
+        print('Lavando la ropa')
+
+    def _centrifugar(self):
+        print('Centrifugando la ropa')
+
+
+def main():
+    lavadora = Lavadora()
+    lavadora.lavar()
+
+
+if __name__ == '__main__':
+    main()
